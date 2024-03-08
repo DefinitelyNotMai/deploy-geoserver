0a763d215f90d0ff5d92f4d3956b6af60a0743f9
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2024-03-08 13:51:54 +0800

    fix: initial update and upgrading with apt

--- geo.sh
+++ geo.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 
-sudo apt update && sudo apt update
+sudo apt update && sudo apt upgrade
 sudo apt install default-jre default-jdk
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
